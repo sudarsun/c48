484a7094bbae7a344fdf22270c0bfc41e81d8279
Rajesh Murugan <btechraj2007@gmail.com>
2016-01-04 23:11:12 +0530

    Updated the alignment for core modules

--- core/AttributeStats.cpp
+++ core/AttributeStats.cpp
@@ -4,24 +4,24 @@
 
 void AttributeStats::addDistinct(double value, int count, double weight) {
 
-	if (count > 0) {
-		if (count == 1) {
-			uniqueCount++;
-		}
-		if (Utils::eq(value, static_cast<double>(static_cast<int>(value)))) {
-			intCount += count;
-		}
-		else {
-			realCount += count;
-		}
-		if (nominalCounts.size() > 0) {
-			nominalCounts[static_cast<int>(value)] = count;
-			nominalWeights[static_cast<int>(value)] = weight;
-		}
-		if (numericStats != nullptr) {
-			numericStats->add(value, weight);
-			numericStats->calculateDerived();
-		}
-	}
-	distinctCount++;
+    if (count > 0) {
+        if (count == 1) {
+            uniqueCount++;
+        }
+        if (Utils::eq(value, static_cast<double>(static_cast<int>(value)))) {
+            intCount += count;
+        }
+        else {
+            realCount += count;
+        }
+        if (nominalCounts.size() > 0) {
+            nominalCounts[static_cast<int>(value)] = count;
+            nominalWeights[static_cast<int>(value)] = weight;
+        }
+        if (numericStats != nullptr) {
+            numericStats->add(value, weight);
+            numericStats->calculateDerived();
+        }
+    }
+    distinctCount++;
 }
--- core/Instance.cpp
+++ core/Instance.cpp
@@ -4,161 +4,161 @@
 
 Instance::Instance(Instance *instance)
 {
-	mAttValues = instance->mAttValues;
-	mWeight = instance->mWeight;
-	mDataset = instance->mDataset;
+    mAttValues = instance->mAttValues;
+    mWeight = instance->mWeight;
+    mDataset = instance->mDataset;
 }
 Instance::Instance(double weight, double_array attValues)
 {
-	mAttValues = attValues;
-	mWeight = weight;
-	mDataset = nullptr;
+    mAttValues = attValues;
+    mWeight = weight;
+    mDataset = nullptr;
 }
 
 Instance* Instance::copy()
 {
-	Instance *result = new Instance(this);
-	result->mDataset = mDataset;
-	return result;
+    Instance *result = new Instance(this);
+    result->mDataset = mDataset;
+    return result;
 }
 Attribute *Instance::attribute(int index)
 {
-	return mDataset->attribute(index);
+    return mDataset->attribute(index);
 }
 
 int Instance::numAttributes()
 {
-	return (int)mAttValues.size();
+    return (int)mAttValues.size();
 }
 int Instance::classIndex()
 {
-	return mDataset->classIndex();
+    return mDataset->classIndex();
 }
 
 void Instance::setValue(int attIndex, double value)
 {
-	freshAttributeVector();
-	mAttValues[attIndex] = value;
+    freshAttributeVector();
+    mAttValues[attIndex] = value;
 }
 
 void Instance::freshAttributeVector()
 {
-	mAttValues = toDoubleArray();
+    mAttValues = toDoubleArray();
 }
 double_array Instance::toDoubleArray()
 {
-	double_array newValues(mAttValues.size());
-	std::copy(std::begin(mAttValues), std::end(mAttValues), std::begin(newValues));
-	return newValues;
+    double_array newValues(mAttValues.size());
+    std::copy(std::begin(mAttValues), std::end(mAttValues), std::begin(newValues));
+    return newValues;
 }
 
 void Instance::setClassMissing()
 {
 
-	int classIndexValue = classIndex();
-	if (classIndexValue < 0)
-	{
-		throw  "Class is not set!";
-	}
-	setMissing(classIndexValue);
+    int classIndexValue = classIndex();
+    if (classIndexValue < 0)
+    {
+        throw  "Class is not set!";
+    }
+    setMissing(classIndexValue);
 }
 
 void Instance::setMissing(int attIndex)
 {
-	setValue(attIndex, Utils::missingValue());
+    setValue(attIndex, Utils::missingValue());
 }
 
 void Instance::setValue(int attIndex, const string &value)
 {
-	int valIndex;
+    int valIndex;
 
-	if (!attribute(attIndex)->isNominal() && !attribute(attIndex)->isString())
-	{
-		throw "Attribute neither nominal nor string!";
-	}
-	valIndex = attribute(attIndex)->indexOfValue(value);
-	if (valIndex == -1)
-	{
-		if (attribute(attIndex)->isNominal())
-		{
-			throw "Value not defined for given nominal attribute!";
-		}
-		else
-		{
-			attribute(attIndex)->forceAddValue(value);
-			valIndex = attribute(attIndex)->indexOfValue(value);
-		}
-	}
-	setValue(attIndex, valIndex);
+    if (!attribute(attIndex)->isNominal() && !attribute(attIndex)->isString())
+    {
+        throw "Attribute neither nominal nor string!";
+    }
+    valIndex = attribute(attIndex)->indexOfValue(value);
+    if (valIndex == -1)
+    {
+        if (attribute(attIndex)->isNominal())
+        {
+            throw "Value not defined for given nominal attribute!";
+        }
+        else
+        {
+            attribute(attIndex)->forceAddValue(value);
+            valIndex = attribute(attIndex)->indexOfValue(value);
+        }
+    }
+    setValue(attIndex, valIndex);
 }
 
 double Instance::value(int attIndex)
 {
-	return mAttValues[attIndex];
+    return mAttValues[attIndex];
 }
 
 double Instance::classValue()
 {
-	int classIndexValue = classIndex();
-	if (classIndexValue < 0)
-	{
-		throw "Class is not set!";
-	}
-	return value(classIndexValue);
+    int classIndexValue = classIndex();
+    if (classIndexValue < 0)
+    {
+        throw "Class is not set!";
+    }
+    return value(classIndexValue);
 }
 
 double Instance::weight()
 {
-	return mWeight;
+    return mWeight;
 }
 
 bool Instance::isMissing(int attIndex)
 {
 
-	if (Utils::isMissingValue(value(attIndex)))
-	{
-		return true;
-	}
-	return false;
+    if (Utils::isMissingValue(value(attIndex)))
+    {
+        return true;
+    }
+    return false;
 }
 
 Attribute *Instance::classAttribute()
 {
 
-	return mDataset->classAttribute();
+    return mDataset->classAttribute();
 }
 
 int Instance::numClasses()
 {
-	return mDataset->numClasses();
+    return mDataset->numClasses();
 }
 
 void Instance::setWeight(double weight)
 {
-	mWeight = weight;
+    mWeight = weight;
 }
 
 void Instance::setDataset(Instances *instances)
 {
-	mDataset = instances;
+    mDataset = instances;
 }
 
 Instances* Instance::getDataset()
 {
-	return mDataset;
+    return mDataset;
 }
 
 double Instance::missingValue()
 {
-	return std::numeric_limits<double>::quiet_NaN();;
+    return std::numeric_limits<double>::quiet_NaN();;
 }
 
 bool Instance::classIsMissing()
 {
-	int classIndexValue = classIndex();
-	if (classIndexValue < 0)
-	{
-		throw "Class is not set!";
-	}
-	return isMissing(classIndexValue);
+    int classIndexValue = classIndex();
+    if (classIndexValue < 0)
+    {
+        throw "Class is not set!";
+    }
+    return isMissing(classIndexValue);
 }
